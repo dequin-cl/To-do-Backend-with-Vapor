ebeb5324545766e9edc98890f3bbe1cd2b9d9299
Iván GalazJeria <dequin@gmail.com>
2020-08-06 00:00:18 -0400

    HTTP2 working

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,7 +6,27 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    app.http.server.configuration.responseCompression = .enabled
+    
+    let homePath = app.directory.workingDirectory
+    let certPath = homePath + "cert/cert.pem"
+    let keyPath = homePath + "cert/key.pem"
+    
+    let certs = try! NIOSSLCertificate.fromPEMFile(certPath)
+        .map { NIOSSLCertificateSource.certificate($0) }
+    let tls = TLSConfiguration.forServer(certificateChain: certs, privateKey: .file(keyPath))
+
+    app.http.server.configuration = .init(hostname: "127.0.0.1",
+                                          port: 8080,
+                                          backlog: 256,
+                                          reuseAddress: true,
+                                          tcpNoDelay: true,
+                                          responseCompression: .disabled,
+                                          requestDecompression: .disabled,
+                                          supportPipelining: false,
+                                          supportVersions: Set<HTTPVersionMajor>([.two]),
+                                          tlsConfiguration: tls,
+                                          serverName: nil,
+                                          logger: nil)
 
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
