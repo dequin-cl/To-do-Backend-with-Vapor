29246f14bcf585b01054ab3aa94396878acd9ca4
Iván GalazJeria <dequin@gmail.com>
2020-10-21 11:24:44 -0300

    AppTests file

---
+++ Tests/AppTests/AppTests.swift
@@ -0,0 +1,34 @@
+@testable import App
+import XCTVapor
+
+final class AppTests: XCTestCase {
+    
+    func testHelloWorld() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "hello") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello, world!")
+        }
+    }
+    
+    func testCreateTodo() throws {
+
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        
+        try app.test(.POST, "todos", beforeRequest: { req in
+            try req.content.encode(
+                Todo(title: "Test", owner: UUID.generateRandom())
+            )
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            let todo = try res.content.decode(Todo.self)
+            XCTAssertEqual(todo.title, "Test")
+        })
+    }
+}
